095c63750f4d547cd039ef4200902a9c95cf94c9
MADHANSHARON <madhanag2017@gmail.com>
2024-08-10 09:26:38 +0530

    added

---
+++ Remove_duplicates_II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        for(int i:nums){
+            if(count<2 || i!=nums[count-2]){
+                nums[count]=i;
+                count++;
+            }
+            
+        }
+        return count;
+    }
+}
